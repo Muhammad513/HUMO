030a849fe012b58e095dd7b6f53ac1a7b7e0dce7
MuhammadBobur <78463001+Muhammad513@users.noreply.github.com>
2023-04-15 13:32:15 +0500

    users

--- login/views.py
+++ login/views.py
@@ -9,7 +9,7 @@ from django.forms import formset_factory,modelformset_factory
 from datetime import datetime
 from login.form import Gallaform,Gallaimzo
 from .decarators import*
-
+from django.contrib.auth.decorators import login_required
 def loginPage(request):
     if request.method=="POST":
         username=request.POST["username"]
@@ -22,6 +22,8 @@ def loginPage(request):
             messages.info(request,"login ёки парол нотугри")    
     return render(request,"login/login.html")
 
+
+@login_required(login_url='login')
 def logoutUser(request):
     logout(request)
     return redirect('login')
@@ -29,7 +31,7 @@ def logoutUser(request):
 
 
 
-
+@login_required(login_url='login')
 def setting(request):
     user=request.user.profile.id
     profile=Profile.objects.get(id=user)
@@ -46,12 +48,13 @@ def setting(request):
 
 
 
-
+@login_required(login_url='login')
 def paxta(request):
     
     
     return render(request,'homes/paxta.html')
 
+@login_required(login_url='login')
 def galla(request):
     date=request.GET.get("date")
     date=datenow(date)
@@ -90,39 +93,40 @@ def galla(request):
     return render(request,'homes/galla/galla.html',context)
 
 
+@login_required(login_url='login')
 def ombor(request):
-    
     dmk1=item_ombor_G(OmborG,"DMK-1")
     dmk2=item_ombor_G(OmborG,"DMK-2")
     zavod=item_ombor_G(OmborG,"ZAVOD")
     context={"dmk1":dmk1,'dmk2':dmk2,"zavod":zavod}
     return render(request,'homes/dmk.html',context)
 
+@login_required(login_url='login')
 def reestr(request):
     res=Galla.objects.all().order_by('-id').values('date','brigada','brutto','tara','ombor__name','yuk_num','tr_num','tr_marka','tr_name','imzo','sofVazn')
     context={'res':res}
     return render(request,'homes/restr.html',context)    
 
 
-
+@login_required(login_url='login')
 def kadr(request):
     kadr=Hodim.objects.all().order_by('bolim')
     
     context={"kadr":kadr}
-    
     return render(request,'kadr/kadr-inc.html',context)        
 
 
+@login_required(login_url='login')
 def tavalud(request):
     dates=datetime.now().strftime("%d.%m")
     tavalud=Hodim.objects.all()
-    
+
     context={"tavalud":tavalud,"dates":dates}
-    
     return render(request,'kadr/tavalud.html',context)        
 
-def gallaform(request):
 
+@allowed_users(allowed_roles=['nazoratchi','admin'])
+def gallaform(request):
     form=Gallaform()
     if request.method == "POST":
         form=Gallaform(request.POST)
@@ -131,12 +135,15 @@ def gallaform(request):
             return redirect('gallaform')
     context={'form':form}
     return render(request,'form/yukhati.html',context)    
-@allowed_users(allowed_roles=['Ombor'])
+
+
+@allowed_users(allowed_roles=['Ombor','admin'])
 def zavodimzo(request):
     zavod=Galla.objects.filter(ombor__name="ZAVOD",imzo=False).order_by('-id').values('date','brigada','ombor__name','yuk_num','tr_num','tr_marka','tr_name','imzo','id')
     context={'zavod':zavod}
     return render(request,'form/zavodimzo.html',context)    
 
+@allowed_users(allowed_roles=['Ombor','admin'])
 def zavodimzopk(request,pk):
     imzo=Galla.objects.get(id=pk)
     form=Gallaimzo(instance=imzo)
@@ -148,11 +155,13 @@ def zavodimzopk(request,pk):
     context={"form":form,"imzo":imzo}
     return render(request,'form/zavodimzopk.html',context)    
 #------------------------------------------------------------------------------------------------
+@allowed_users(allowed_roles=['dmk','admin'])
 def dmkimzo(request):
     dmk=Galla.objects.filter(ombor__name__in=['DMK-1',"DMK-2"],imzo=False).order_by('-id').values('date','brigada','ombor__name','yuk_num','tr_num','tr_marka','tr_name','imzo','id')
     context={'dmk':dmk}
     return render(request,'form/dmkimzo.html',context)    
 
+@allowed_users(allowed_roles=['dmk','admin'])
 def dmkimzopk(request,pk):
     imzo=Galla.objects.get(id=pk)
     form=Gallaimzo(instance=imzo)
